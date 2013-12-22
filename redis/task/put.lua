b7769ca7db65b68e365b298065db08c300edc0ac
0x20h <kohj@informatik.uni-marburg.de>
2013-12-22 07:18:22 +0100

    fix

--- redis/task/put.lua
+++ redis/task/put.lua
@@ -13,7 +13,7 @@ ARGS:
 local channel, task_key, task, schedule = KEYS[1], KEYS[2], ARGV[1], ARGV[2]
 
 -- store task
-redis.call('hmset', task_key, 'task', task, 'channel', channel);
+redis.call('hmset', task_key, 'task', task, 'channel', channel)
 -- push task to channel
 redis.call('lpush', channel, task_key)
 return 1
