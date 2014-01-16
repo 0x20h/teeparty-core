2c49fa43fca2a94255875fec8cb95e5521f90ba2
0x20h <kohj@informatik.uni-marburg.de>
2014-01-16 06:49:01 +0100

    added ability to put task for a specific execution time

--- redis/task/put.lua
+++ redis/task/put.lua
@@ -9,15 +9,22 @@ ARGS:
     - the channel
     - task id.
     - json-encoded task.
+    - execution time (OPTIONAL)
 ]]--
 
-local prefix, channel, task_id, task = 
-    ARGV[1], ARGV[2], ARGV[3], ARGV[4]
+local prefix, channel, task_id, task, execution_time =
+    ARGV[1], ARGV[2], ARGV[3], ARGV[4], ARGV[5]
 
 local task_key = prefix .. 'task.' .. task_id
 
 -- store task
 redis.call('hmset', task_key, 'task', task, 'channel', channel)
--- push task to channel
-redis.call('lpush', prefix .. 'channel.' .. channel, task_id)
+if execution_time then
+    -- schedule task for later execution
+    redis.call('zadd', prefix .. 'scheduler', execution_time, task_id)
+else
+    -- push task to channel for ASAP execution
+    redis.call('lpush', prefix .. 'channel.' .. channel, task_id)
+end
+
 return 1
