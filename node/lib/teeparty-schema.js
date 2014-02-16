9165c81a0c5eeacdab057e3d33d9cfb3bc7737b7
0x20h <kohj@informatik.uni-marburg.de>
2014-02-16 09:05:12 +0100

    basic schema validation for node

---
+++ node/lib/teeparty-schema.js
@@ -0,0 +1,30 @@
+var JaySchema = require('jayschema'),
+    validator = new JaySchema,
+    fs = require('fs'),
+    events = require('events'),
+    path = require('path')
+
+module.exports.validate = validate
+
+function validate(schemafile, json, fn) {
+  if (!(this instanceof validate)) {
+      return new validate(schemafile, json, fn)
+  }
+
+  var self = this
+  events.EventEmitter.call(this)
+  var base = path.resolve([__dirname, '..', '..', 'schema'].join('/'))
+
+  fs.readFile([base, schemafile + '.json'].join('/'), function(err, data) {
+    if (err) {
+      self.emit('error', err)
+    } else {
+      var schema = JSON.parse(data.toString())
+      validator.validate(json, schema, function(errs) {
+        fn(errs)
+      })
+    }
+  })
+}
+
+validate.prototype.__proto__ = events.EventEmitter.prototype
---
+++ node/tests/schema.test.js
@@ -0,0 +1,48 @@
+var assert = require('assert'),
+    schema = require('./../lib/teeparty-schema')
+
+describe('schema', function() {
+  it('should fail on non-existing schema', function(done) {
+    schema.validate('non-existant', '', function(err) {
+    }).on('error', function(err) {
+      done()
+    })
+  })
+
+  it('should validate a valid task', function(done) {
+    var now = new Date
+
+    var task = {
+      job: 'someJob',
+      meta: {
+        id: 'a12fab',
+        created: now.toISOString(),
+        max_tries: 3,
+        tries: 0
+      }
+    }
+
+    schema.validate('task', task, function(err) {
+      if (err) {
+        console.error(err)
+      } else {
+        done()
+      }
+    })
+  })
+
+  it('should report correct error on invalid task', function(done) {
+    var task = {
+      job: 'foo'
+      // meta missing
+    }
+
+    schema.validate('task', task, function(err) {
+      if (err) {
+        assert.equal(err[0].constraintName, 'required')
+        assert.equal(err[0].desc, 'missing: meta')
+        done()
+      }
+    })
+  })
+})
